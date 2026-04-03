4b5dbeabac318d801c2402fd28237911cc486444
uidls31 <lagutin7234@gmail.com>
2026-04-03 14:17:44 +0200

    Initial Commit

---
+++ PromptManager/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PromptManager
+//
+//  Created by Александр Лагутин on 03/04/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PromptManager/PromptManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PromptManagerApp.swift
+//  PromptManager
+//
+//  Created by Александр Лагутин on 03/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct PromptManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
